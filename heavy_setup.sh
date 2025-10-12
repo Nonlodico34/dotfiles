ab40611152286491e0b8d62cb0793df2c6d23160
Nonlodico34 <luca.mor.dm@gmail.com>
2025-10-12 23:49:13 +0200

    Aggiunto script heavy setup da lanciare per installare tutto su una nuova macchina

---
+++ heavy_setup.sh
@@ -0,0 +1,21 @@
+sudo apt-get install nala -y
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install fzf -y
+sudo apt install bat -y
+sudo apt install zoxide -y
+sudo apt install tmux -y
+sudo apt install eza -y
+sudo apt install ranger -y
+sudo apt install build-essential -y
+sudo apt install ncdu -y
+sudo apt install fd-find -y
+sudo apt install tty-clock -y
+sudo apt install bpytop -y
+
+# # install fzf
+# cd ~
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# ~/.fzf/install
+# rm -rf ~/.fzf
