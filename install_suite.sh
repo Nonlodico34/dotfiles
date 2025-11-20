81f59117bff7b9373751431c43b548d208b90b8c
Nonlodico34 <112514312+Nonlodico34@users.noreply.github.com>
2025-11-20 12:24:02 +0100

    Update install_suite.sh with new packages
    Added installation commands for micro, unzip, and updated nala installation.

--- install_suite.sh
+++ install_suite.sh
@@ -1,9 +1,8 @@
-sudo apt-get install nala -y
-echo "alias apt='nala'" >> ~/.bashrc
-source ~/.bashrc
 sudo apt update
 sudo apt upgrade -y
 
+sudo apt install nala -y
+sudo apt install micro -y
 sudo apt install bat -y
 sudo apt install zoxide -y
 sudo apt install tmux -y
@@ -12,6 +11,9 @@ sudo apt install ranger -y
 sudo apt install build-essential -y
 sudo apt install ncdu -y
 sudo apt install bpytop -y
+sudo apt install unzip -y
+curl -s https://ohmyposh.dev/install.sh | bash
+source ~/.bashrc
 
 # # install fzf
 # cd ~
