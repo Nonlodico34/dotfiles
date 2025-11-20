d6b3ccb3fac96e4ef946bfbadbc83786fbc71262
Nonlodico34 <112514312+Nonlodico34@users.noreply.github.com>
2025-11-20 11:59:56 +0100

    Modify heavy_setup.sh to change package manager alias
    Removed installation of fzf and fd-find. Added alias for apt to use nala.

--- heavy_setup.sh
+++ heavy_setup.sh
@@ -1,8 +1,9 @@
 sudo apt-get install nala -y
+echo "alias apt='nala'" >> ~/.bashrc
+source ~/.bashrc
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install fzf -y
 sudo apt install bat -y
 sudo apt install zoxide -y
 sudo apt install tmux -y
@@ -10,8 +11,6 @@ sudo apt install eza -y
 sudo apt install ranger -y
 sudo apt install build-essential -y
 sudo apt install ncdu -y
-sudo apt install fd-find -y
-sudo apt install tty-clock -y
 sudo apt install bpytop -y
 
 # # install fzf
